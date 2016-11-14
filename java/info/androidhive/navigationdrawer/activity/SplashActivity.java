060c1f8391175b49181c1512688e88be3fe53fb9
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-11-14 21:21:47 +0700

    Added splash screen

---
+++ java/info/androidhive/navigationdrawer/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package info.androidhive.navigationdrawer.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(1000);
+                } catch (Exception e) {
+
+                } finally {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
